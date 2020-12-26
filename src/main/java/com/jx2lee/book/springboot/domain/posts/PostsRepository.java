e5d429ce1137ebc89cbda31de527fd1a4b32bcd0
jx2lee <jaejun.lee.1991@gmail.com>
2020-12-26 17:30:20 +0900

    from p.100

---
+++ src/main/java/com/jx2lee/book/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,7 @@
+package com.jx2lee.book.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long>{
+
+}
