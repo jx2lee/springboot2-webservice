22807e52795a8933f6155a96b94c759d97471534
jx2lee <jaejun.lee.1991@gmail.com>
2021-04-07 16:43:57 +0900

    added: scripts

---
+++ scripts/deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+REPOSITORY=/app/jx2lee-springboot2-webservice
+PROJECT_NAME=jx2lee-springboot2-webservice
+
+echo "> 현재 구동중인 애플리케이션 pid 확인"
+
+CURRENT_PID=$(pgrep -fl freelec-springboot2-webservice | grep jar | awk '{print $1}')
+
+echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
+fi
+
+echo "> 새 어플리케이션 배포"
+
+JAR_NAME=$(ls -tr $REPOSITORY/build/libs/*.jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+echo "> $JAR_NAME 에 실행권한 추가"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+nohup java -jar \
+    -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,$REPOSITORY/application-oauth.properties \
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
